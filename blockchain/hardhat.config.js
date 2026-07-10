630773ddf745a8154cf27da07a99dc17dc2d28df
Nisarg Vadechiya <nisargvadechiya@gmail.com>
2026-07-10 11:00:19 +0530

    Configure hardhat for Sepolia

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -10,6 +11,10 @@ module.exports = {
     ganache: {
       url: "http://127.0.0.1:7545",
       chainId: 1337,
+    },
+    sepolia: {
+      url: process.env.ETH_RPC_URL || "",
+      accounts: process.env.ETH_PRIVATE_KEY ? [process.env.ETH_PRIVATE_KEY] : [],
     }
   }
 };
